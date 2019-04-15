e8b28b38ab78774c2fe59638deff4afef49b13a4
Logon PF Fiap <logonpflocal@fiap.com.br>
2019-04-15 20:46:37 -0300

    JPQL

--- 07-JPA-JPQL-Apostila/src/br/com/fiap/dao/ClienteDAO.java
+++ 07-JPA-JPQL-Apostila/src/br/com/fiap/dao/ClienteDAO.java
@@ -1,7 +1,16 @@
 package br.com.fiap.dao;
 
+import java.util.List;
 import br.com.fiap.entity.Cliente;
 
 public interface ClienteDAO extends GenericDAO<Cliente,Integer>{
 
+	List<Cliente> listar();
+	
+	List<Cliente> buscarPorNome(String nome);
+	
+	List<Cliente> buscarPorEstado(String estado);
+	
+	List<Cliente> buscarPorDiasReserva(int dias);
+	
 }
--- 07-JPA-JPQL-Apostila/src/br/com/fiap/dao/impl/ClienteDAOImpl.java
+++ 07-JPA-JPQL-Apostila/src/br/com/fiap/dao/impl/ClienteDAOImpl.java
@@ -1,6 +1,9 @@
 package br.com.fiap.dao.impl;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import br.com.fiap.dao.ClienteDAO;
 import br.com.fiap.entity.Cliente;
@@ -11,4 +14,40 @@ public class ClienteDAOImpl extends GenericDAOImpl<Cliente,Integer> implements C
 		super(entityManager);
 	}
 
+	@Override
+	public List<Cliente> listar() {
+		TypedQuery<Cliente> query = 
+			em.createQuery("from Cliente",Cliente.class);
+		query.setMaxResults(3);
+		return query.getResultList();
+	}
+
+	@Override
+	public List<Cliente> buscarPorNome(String nome) {
+		return em.createQuery("from Cliente c where c.nome "
+				+ "like :name",Cliente.class)
+				.setParameter("name", "%" + nome + "%")
+				.getResultList();
+	}
+
+	@Override
+	public List<Cliente> buscarPorEstado(String estado) {
+		return em.createQuery("from Cliente c where"
+			+ " c.endereco.cidade.uf = :P",Cliente.class)
+			.setParameter("P", estado)
+			.getResultList();
+	}
+
+	@Override
+	public List<Cliente> buscarPorDiasReserva(int dias) {
+		return em.createQuery("select r.cliente from Reserva r where "
+				+ "r.numeroDias = :d",Cliente.class)
+				.setParameter("d", dias)
+				.getResultList();
+	}
+
 }
+
+
+
+
